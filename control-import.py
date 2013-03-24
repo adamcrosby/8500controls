8a6a52cf849c8e0fefb9684228af32bb16be4f76
Adam Crosby <adam@uptill3.com>
2013-03-24 01:59:35 -0400

    add sqlite import

--- control-import.py
+++ control-import.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
-import sys
+import sys, sqlalchemy
+from sqlalchemy.exc import IntegrityError
+from database import db_session, init_db
+from model import Control
+
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -13,23 +17,28 @@ controlsXML = ET.parse(sys.argv[1])
 importFile = controlsXML.getroot()
 
 iaControls = importFile.findall("{%s}IACONTROL" % xmlns)
+init_db()
 
 for control in iaControls:
-	title = control.find("{%s}CONTROL_NUMBER" % xmlns).get('title')
-	number = control.find("{%s}CONTROL_NUMBER" % xmlns).text
-	MACs = [mac.text for mac in control.findall("{%s}MACCONF/{%s}MAC" % (xmlns, xmlns))]
-	CONFs = [conf.text for conf in control.findall("{%s}MACCONF/{%s}CONF" % (xmlns, xmlns))]
-	name = control.find("{%s}CONTROL_NAME" % xmlns).text
-	if name != title:
-			print "number: %s" % number
+	c = Control()
+	c.title = control.find("{%s}CONTROL_NUMBER" % xmlns).get('title')
+	c.number = control.find("{%s}CONTROL_NUMBER" % xmlns).text
+	c.MAC = '|'.join([mac.text for mac in control.findall("{%s}MACCONF/{%s}MAC" % (xmlns, xmlns))])
+	c.CONF = '|'.join([conf.text for conf in control.findall("{%s}MACCONF/{%s}CONF" % (xmlns, xmlns))])
+	c.name = control.find("{%s}CONTROL_NAME" % xmlns).text
+	c.subjectArea = control.find("{%s}SUBJECT_AREA" % xmlns).text
+	c.impactCode = control.find("{%s}IMPACT_CODE" % xmlns).text
+	c.description = control.find("{%s}DESCRIPTION" % xmlns).text
+	c.threat = control.find("{%s}THREAT" % xmlns).text
+	c.guidance = control.find("{%s}GENERAL_IMPLEMENTATION_GUIDANCE" % xmlns).text
+	c.references = control.find("{%s}SYSTEM_SPECIFIC_GUIDANCE_RESOURCES" % xmlns).text
 
 
+	db_session.add(c)
+db_session.commit()
 
 
-
-
-
-test = """
+test = u"""
 <IMPORT_FILE xmlns="urn:FindingImport">
   <IACONTROL>
     <CONTROL_NUMBER title="Alternate Site Designation">COAS-1</CONTROL_NUMBER>
@@ -37,5 +46,8 @@ test = """
       <MAC>MACIII</MAC>
     </MACCONF>
     <CONTROL_NAME>Alternate Site Designation</CONTROL_NAME>
-datestr = benchmark.find("{%s}status" % xmlns).get("date")
-"""
+    <SUBJECT_AREA>Continuity</SUBJECT_AREA>
+    <IMPACT_CODE>High</IMPACT_CODE>
+    <DESCRIPTION>An alternate site is identified that permits the restoration of all mission or business essential functions.</DESCRIPTION>
+    <THREAT>Environmental disasters, organized disruptions, loss of utilities/services, equipment or system failures, and serious information security incidents are potential events that could disrupt mission or business essential functions.  A recovery strategy should be developed to include an alternate site to mitigate the impact of disruptive events.</THREAT>
+    <GENERAL_IMPLEMENTATION_GUIDANCE/><SYSTEM_SPECIFIC_GUIDANCE_RESOURCES/>"""
